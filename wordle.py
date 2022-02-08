05e6003d88e75c04730b28b07346840dfeff1775
Samuel <samuel.laws@protonmail.com>
2022-02-08 11:46:41 +0000

    wordcli

--- wordle.py
+++ wordle.py
@@ -1,7 +1,10 @@
-from tinge import colored, success
-import itertools
-import argparse
 import random
+import string
+import argparse
+import itertools
+import pandas as pd
+from tinge import colored
+from pandas.api.types import CategoricalDtype
 
 
 class Wordle:
@@ -25,23 +28,46 @@ class Wordle:
             if guess not in self.word_list:
                 self.print_guess(self.guess_results)
                 if len(guess) < self.num_letters:
-                    print("Too short!")
+                    print("\nToo short!")
                 elif len(guess) > self.num_letters:
-                    print("Too long!")
+                    print("\nToo long!")
                 else:
-                    print("Invalid guess!")
+                    print("\nInvalid guess!")
 
             elif (guess in self.word_list) & (guess != self.word):
                 self.guess_count += 1
                 guess_letters = list(map(''.join, zip(*[iter(guess)]*1)))
+                guess_index = 0
                 for l in guess_letters:
                     gidx = guess_letters.index(l)
+
                     if l in self.word_letters:
                         lidx = self.word_letters.index(l)
-                        if lidx == gidx:
-                            self.guess_results.append((l.upper(), 'green'))
-                        else:
-                            self.guess_results.append((l.upper(), 'yellow'))
+                        if len([x for x in guess_letters if x == l]) == 1:
+                            if lidx == gidx:
+                                self.guess_results.append((l.upper(), 'green'))
+                            else:
+                                self.guess_results.append(
+                                    (l.upper(), 'yellow'))
+                        elif len([x for x in guess_letters if x == l]) > 1:
+                            lidxs = [index for index, element in enumerate(
+                                self.word_letters) if element == l]
+                            gidxs = [index for index, element in enumerate(
+                                guess_letters) if element == l]
+                            gidx = gidxs[guess_index]
+
+                            try:
+                                lidxs_guess_indexed = lidxs[guess_index]
+                            except IndexError:
+                                lidxs_guess_indexed = lidx
+                            if lidxs_guess_indexed == gidxs[guess_index]:
+                                self.guess_results.append((l.upper(), 'green'))
+                            elif gidx == lidxs[0]:
+                                self.guess_results.append(
+                                    (l.upper(), 'yellow'))
+                            else:
+                                self.guess_results.append((l.upper(), 'white'))
+                            guess_index += 1
                     else:
                         self.absent_letters.append(l.upper())
                         self.guess_results.append((l.upper(), 'white'))
@@ -53,16 +79,40 @@ class Wordle:
                     self.guess_results.append((l.upper(), 'green'))
                 self.print_guess(self.guess_results)
 
-                print(colored("\nYou got it!", "green"))
+                print(colored("\n\nYou got it!", "green"))
 
     def print_guess(self, guess_results):
         print("", end="\r\r")
+        alphabet = list(string.ascii_uppercase)
+        guess_results_copy = guess_results.copy()
+        for letter in alphabet:
+            guess_results_copy.append((letter, "white"))
+        guess_df = pd.DataFrame(
+            guess_results_copy, columns=["letter", "status"])
+        guess_df.drop_duplicates(inplace=True)
+        sorted_guesses = self.sort_guesses(guess_df)
+
         [print(colored(x[0], x[1]), end="") for x in guess_results]
+        print()
+        [print(colored(x[0], x[1]), end="") for x in sorted_guesses]
+        print()
+
+    def sort_guesses(self, guess_df):
+        cat_size_order = CategoricalDtype(
+            ["white", "yellow", "green"],
+            ordered=True
+        )
+        guess_df['status'] = guess_df['status'].astype(cat_size_order)
+        guess_df = guess_df.sort_values(by="status", ascending=False)
+        guess_df.drop_duplicates(subset="letter", keep="first", inplace=True)
+        guess_df = guess_df[guess_df["letter"] != "\n"]
+        guess_df_sorted = guess_df.sort_values(by="letter", ascending=True)
+        sorted_guesses = list(guess_df_sorted.to_records(index=False))
+        return sorted_guesses
 
     def request_absent_letters(self):
         self.absent_letters = list(dict.fromkeys(self.absent_letters))
         self.absent_letters.sort()
-        print("".join(self.absent_letters))
 
 
 def parse_args(args=None):
@@ -96,7 +146,7 @@ def main(args=None):
             break
 
     if not success:
-        print(colored("\nGame over punk!", "red"))
+        print(colored("Game over punk!", "red"))
         print(f"The word was: {game.word}")
 
 
